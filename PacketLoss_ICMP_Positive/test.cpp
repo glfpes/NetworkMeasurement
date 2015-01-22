7e8d2d1b0f5beb8b82d3e27f7169737d3694a605
Yang Guang <glfpes@sjtu.edu.cn>
2015-01-22 21:46:28 +0800

    增加了主函数的功能
    主函数接收3个参数，并将结果写入txt文件中，这样主函数可以为BS架构的核心网站控制并提供服务数据了。

--- PacketLoss_ICMP_Positive/test.cpp
+++ PacketLoss_ICMP_Positive/test.cpp
@@ -1,21 +1,48 @@
 #include "PacketLoss_ICMP_Positive.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	int num_of_para = argc;
+	char* OS_string = argv[1];
+	char* IP = argv[2];
+	char* num_to_send_string = argv[3];
+	
+	int OS = atoi(OS_string);
+	int num = atoi(num_to_send_string);
+	cout<<OS<<endl<<IP<<endl<<num<<endl;
 
+	PacketLoss_ICMP_Positive PacketLoss_ICMP(OS, num, IP);
+	float average_delay = PacketLoss_ICMP.get_average_latency();
+	float jitter = PacketLoss_ICMP.get_jitter();
+	float loss_rate = PacketLoss_ICMP.get_loss_rate();
+	bool av = PacketLoss_ICMP.isAvailable();
 
+	ofstream out;
+	out.open("result.txt");
+	out<<average_delay<<endl
+		<<jitter<<endl
+		<<loss_rate<<endl
+		<<av<<endl;;
+
+	cout<<"Average latency is "<<average_delay<<endl;
+	cout<<"Jitter(Standard deviation) is "<<jitter<<endl;
+	cout<<"Packet Loss Rate is "<<loss_rate<<endl;
+	cout<<"Availablity is "<<av<<endl;
+
+	/*
+	cout<<"Please select your OS, 0 for windows and 1 for linux: "<<endl;
+	int OS = 0;
+	cin>>OS;
 	cout<<"Please input the Destination you want to test: "<<endl;
     string IP("");
     cin>>IP;
 	cout<<"Please input the number of ICMP packets you want to test: "<<endl;
 	int num = 4;
 	cin>>num;
-	cout<<"Please select your OS, 0 for windows and 1 for linux: "<<endl;
-	int OS = 0;
-	cin>>OS;
+
 
 	PacketLoss_ICMP_Positive PacketLoss_ICMP(OS, num, IP);
-
+	*/
 
 /*
 	int OS = 0;
@@ -23,7 +50,7 @@ int main()
 	string IP = "www.baidu.com";
 	PacketLoss_ICMP_Positive PacketLoss_ICMP(OS, num, IP);
 */	
-	
+	/*
 	float average_delay = PacketLoss_ICMP.get_average_latency();
 	cout<<"Average latency is "<<average_delay<<endl;
 	float jitter = PacketLoss_ICMP.get_jitter();
@@ -33,8 +60,8 @@ int main()
 	bool av = PacketLoss_ICMP.isAvailable();
 	cout<<"Packet Loss Rate is "<<loss_rate<<endl;
 	cout<<"Availablity is "<<av<<endl;
-
-
+	*/
+	
 
 	return 0;
 }
